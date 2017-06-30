9920f97aecdd59484e24747c703af62aea547925
afractal <hermesxgjini@gmail.com>
2017-06-30 12:48:37 +0200

    publish to heroku -g

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+var path = require('path');
 var express = require('express');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 5000);
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+
+// app.listen(app.get('port'), function () {
+//     console.log('Node app is running on port', app.get('port'));
+// });
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
