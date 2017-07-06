2d7459a80eeccb872f242a3cc9d71763d8fca78d
afractal <hermesxgjini@gmail.com>
2017-07-06 10:36:36 +0200

    update apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
     production: true,
-    apiUrl: 'http://maboroshi.azurewebsites.net/api/'
+    apiUrl: 'https://daze-api.herokuapp.com/api/'
 };
