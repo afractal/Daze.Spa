10ef0d879beff50ae7e6576a8184120bb79c55a0
afractal <hermesxgjini@gmail.com>
2017-10-23 22:52:59 +0200

    add gzip compression

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var path = require('path');
 var express = require('express');
+var compression = require('compression');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, './dist')));
 
 app.listen(app.get('port'), function () {
